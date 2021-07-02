4024fd5f20e04ab03afea35b69ef7bdf8e654f30
Ethan Yeh <ethan.yeh@cyrdg.com>
2021-07-03 03:08:11 +0800

    refactor: assign aws profile via environment variable

--- main.tf
+++ main.tf
@@ -16,7 +16,6 @@ terraform {
   }
 }
 provider "aws" {
-  profile = var.aws_profile
   region = "ap-northeast-1"
 }
 
--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@ variable "token" {
   description = "GitHub access token used to configure the provider"
   type        = string
 }
-
-variable "aws_profile" {
-  description = "aws cli profile"
-  type        = string
-}
